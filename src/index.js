54017241450ff3e8e464f628012ba033e23a4176
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2024-08-01 18:30:15 +0200

    Added icon forecast from API, changed CSS

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let weatherIconElement = document.querySelector("#icon");
 
-  console.log(response.data);
+  weatherIconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
-  timeElement.innerHTML = formatDate(date);
 }
 
 function formatDate(date) {
