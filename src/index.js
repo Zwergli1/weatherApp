41a314634151f751d513baa315a738974fde57b5
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2024-08-01 17:40:10 +0200

    Added forecast wind speed from API

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#weather-app-city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
 
+  console.log(response.data);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
 }
 
 function searchCity(city) {
