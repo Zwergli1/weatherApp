bbf68f2807c52557f5625ac825c705be4cad6feb
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2024-08-04 12:00:57 +0200

    Inserted the forecast API in each section

--- src/index.js
+++ src/index.js
@@ -54,25 +54,31 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "c945b1b3a38a7ed39dtf309fbc9oc934";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="weather-forecast-day">
-      <div class="weather-forecast-date">Tue</div>
-      <div class="weather-forecast-icon">
-      <img src="${day.condition.icon_url}" />
-      </div>
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+      
       <div class="weather-forecast-temperatures">
         <div class="weather-forecast-temperature">
           <strong>${Math.round(day.temperature.maximum)}°C</strong>
@@ -83,6 +89,7 @@ function displayForecast(response) {
       </div>
     </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
